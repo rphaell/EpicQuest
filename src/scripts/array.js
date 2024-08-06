2f514f4c02f98389ceca54df2c39be954c06412c
Raphael <phael92br@gmail.com>
2024-08-06 14:04:15 -0300

    att responsividade do background do hero

---
+++ src/scripts/array.js
@@ -0,0 +1,5 @@
+const redesSociais  = ['facebook', 'Instagram', 'Twitter'];
+
+for (let i = 0; i < redesSociais.length; i++) {
+    console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`)
+}
